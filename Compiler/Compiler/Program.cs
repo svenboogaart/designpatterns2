f56d931d6843bd653c9b8bdddb6cba89d751a8de
svenboogaart <svenboogaart@Gmail.com>
2015-09-10 12:00:41 +0200

    TOkenizer

--- Compiler/Compiler/Program.cs
+++ Compiler/Compiler/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace Compiler
     {
         static void Main(string[] args)
         {
+
+            string fileName = Console.ReadLine();
+            if (fileName.Equals(""))
+                fileName = "C:\\CompilerText.txt";
+
+            try
+            {
+                string textFromFile = System.IO.File.ReadAllText(@fileName);
+                Tokenizer tokenizer = new Tokenizer();
+                tokenizer.Tokenize(textFromFile);
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Kan bestand niet openen:");
+                Console.WriteLine(e.Message);
+            }
+
+            
+
+            Console.WriteLine("Press any key to exit.");
+            System.Console.ReadKey();
+
+
+            
         }
     }
 }
---
+++ Compiler/Compiler/Tokenizer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler
+{
+    class Tokenizer
+    {
+        public void Tokenize(String input)
+        {
+            input = input.Replace(System.Environment.NewLine, "");
+            Console.WriteLine("TOKENIZE");
+            Console.WriteLine();
+            string[] words = input.Split(new string[] { "einde" }, StringSplitOptions.None);
+            foreach (string word in words)
+            {
+
+                Console.WriteLine("-- "+word.Trim());
+                string[] parts = word.Split(new string[] { " " }, StringSplitOptions.None);
+                foreach (string part in parts)
+                {
+                    checkToken(part.Trim());
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("END TOKENIZE");
+            Console.WriteLine();
+        }
+
+        public void checkToken(String token)
+        {
+            switch (token)
+            {
+                case "begincode":
+                    Console.WriteLine("{");
+                    break;
+                case "eindcode":
+                    Console.WriteLine("}");
+                    break;
+                case "als":
+                    Console.WriteLine("if");
+                    break;
+                case "andersals":
+                    Console.WriteLine("else if");
+                    break;
+                case "anders":
+                    Console.WriteLine("else");
+                    break;
+                case "woord":
+                    Console.WriteLine("string");
+                    break;
+                case "getal":
+                    Console.WriteLine("int");
+                    break;
+                case "letter":
+                    Console.WriteLine("string");
+                    break;
+                case "wordt":
+                    Console.WriteLine("=");
+                    break;
+                case "is":
+                    Console.WriteLine("==");
+                    break;
+                case "verhogen":
+                    Console.WriteLine("++");
+                    break;
+                case "verlagen":
+                    Console.WriteLine("--");
+                    break;
+                default:
+                    Console.WriteLine(token);
+                    break;
+            }
+        }
+    }
+}
